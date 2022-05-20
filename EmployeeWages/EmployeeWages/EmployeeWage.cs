012f8517eb95ea898dba04f65e7e7c8ad743847b
rohantare03 <rohatare123@gmail.com>
2022-05-20 10:24:45 +0530

    [Rohan] Add : Checking Daily Employee wages

--- EmployeeWages/EmployeeWages/EmployeeWage.cs
+++ EmployeeWages/EmployeeWages/EmployeeWage.cs
@@ -9,8 +9,9 @@ namespace EmployeeWages
     internal class EmployeeWage
     {
         public int EmpPresent = 1;
-
-        public void CheckEmpPresentAbsent()
+        public int EmpHr = 8;
+        public int WagesPerHr = 20;
+        public void DailyWage()
         {
             Random check = new Random();
             int CheckEmp = check.Next(0, 2);
@@ -18,6 +19,8 @@ namespace EmployeeWages
             if (EmpPresent == CheckEmp)
             {
                 Console.WriteLine("Employee is Present");
+                int DailyWagePerHr = EmpHr * WagesPerHr;
+                Console.WriteLine("Dailywages :" + DailyWagePerHr);
             }
             else
             {
