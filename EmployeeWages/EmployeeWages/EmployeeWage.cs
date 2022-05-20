dc4eed8cb5918bb96e6a8257498eea024e17fb7c
rohantare03 <rohatare123@gmail.com>
2022-05-20 10:20:08 +0530

    [Rohan] Add : Checking if Employee is present or absent

---
+++ EmployeeWages/EmployeeWages/EmployeeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWage
+    {
+        public int EmpPresent = 1;
+
+        public void CheckEmpPresentAbsent()
+        {
+            Random check = new Random();
+            int CheckEmp = check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EmployeeWages
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.CheckEmpPresentAbsent(); 
+        }
+    }
+}
